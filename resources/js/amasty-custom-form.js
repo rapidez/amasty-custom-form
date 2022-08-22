ebf497ae0a902e792766e1ef53a651db5d917c98
indykoning <15870933+indykoning@users.noreply.github.com>
2022-08-22 15:31:33 +0200

    Replace variables with the variable value (#2)

--- resources/js/amasty-custom-form.js
+++ resources/js/amasty-custom-form.js
@@ -17,6 +17,20 @@ Vue.prototype.transformAmCustomFormResponse = async function (response) {
 
 Vue.prototype.amFormToVariables = function (form) {
     let variables = {form_data: {form_id: form.form_id}}
+    let replaceVariables = value => value
+        // Customer Variables
+        .replaceAll('{firstname}', window.app?.user?.firstname ?? '')
+        .replaceAll('{lastname}', window.app?.user?.lastname ?? '')
+        .replaceAll('{email}', window.app?.user?.email ?? '')
+        .replaceAll('{company}', window.app?.user?.addresses[0]?.company ?? '')
+        .replaceAll('{telephone}', window.app?.user?.addresses[0]?.telephone ?? '')
+        .replaceAll('{street}', window.app?.user?.addresses[0]?.street?.join(' ') ?? '')
+        .replaceAll('{city}', window.app?.user?.addresses[0]?.city ?? '')
+        .replaceAll('{region}', window.app?.user?.addresses[0]?.region?.region ?? '')
+        .replaceAll('{postcode}', window.app?.user?.addresses[0]?.postcode ?? '')
+        // General Variables
+        .replaceAll('{url}', window.location.href)
+        .replaceAll('{product_url}', window.location.href);
 
     form.form_json.map(form => {
         form.map(field => {
@@ -25,7 +39,7 @@ Vue.prototype.amFormToVariables = function (form) {
             }
             let value;
             if (value = field.value) {
-                variables.form_data[field.name] = value
+                variables.form_data[field.name] = replaceVariables(value)
             }
 
             if (value = field.values) {
