7b183f77142727f2934a813b7e9818171b5f31ac
Jade Geels <jade@justbetter.nl>
2023-10-05 07:48:50 +0200

    Extra function getUserVariable

--- resources/js/callbacks.js
+++ resources/js/callbacks.js
@@ -20,50 +20,51 @@ Vue.prototype.transformAmCustomFormResponse = async function (response) {
 Vue.prototype.amFormToVariables = function (form) {
     let variables = {form_data: {form_id: form.form_id}}
 
-    let replaceVariables = (value) => {
-        let navigate = (pointer, part) => {
-            if(part.startsWith('custom:')) {
-                return pointer.custom_attributes?.find(attribute => attribute.attribute_code === part.substring(7)).value
-            }
-            switch(part) {
-                case 'address':
-                    return pointer.addresses[0]
-                case 'billing':
-                case 'shipping':
-                    let id = pointer['default_'+part]
-                    return pointer.addresses.find(address => address.id == id)
-                case 'street':
-                    return pointer.street?.join(' ')
+    let navigate = (pointer, part) => {
+        if(part.startsWith('custom:')) {
+            return pointer.custom_attributes?.find(attribute => attribute.attribute_code === part.substring(7)).value
+        }
+        switch(part) {
+            case 'address':
+                return pointer.addresses[0]
+            case 'billing':
+            case 'shipping':
+                let id = pointer['default_'+part]
+                return pointer.addresses.find(address => address.id == id)
+            case 'street':
+                return pointer.street?.join(' ')
 
-                default:
-                    return pointer[part]
-            }
+            default:
+                return pointer[part]
+        }
+    }
+
+    let getUserVariable = (parts) => {
+        let pointer = window.app?.user
+        if(!pointer) {
+            return ''
         }
 
+        // Iterate through the parts to navigate the user object
+        for(const part of parts) {
+            pointer = navigate(pointer, part)
+
+            // Return empty string when not defined
+            if(!pointer) {
+                return ''
+            }
+        }
+        return pointer;
+    }
+    
+    let replaceVariables = (value) => {
         // Get all {}-enclosed variables and replace them
         value = value.replaceAll(/(?<replace>{(?<variable>[^\}]*)})/g, (match, replace, input) => {
             switch(input) {
                 case '{product_url}':
                 case '{url}': return window.location.href
 
-                default:
-                    let parts = input.split('.')
-
-                    let pointer = window.app?.user
-                    if(!pointer) {
-                        return ''
-                    }
-
-                    // Iterate through the parts to navigate the user object
-                    for(const part of parts) {
-                        pointer = navigate(pointer, part)
-
-                        // Return empty string when not defined
-                        if(!pointer) {
-                            return ''
-                        }
-                    }
-                    return pointer;
+                default: return getUserVariable(input.split('.'))                    
             }
         })
 
